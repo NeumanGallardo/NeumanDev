0e3dad2d92de20f97e341963466b75a99cd1a2e6
Erik Neuman Gallardo Castillo <neuman10327033@gmail.com>
2025-04-16 00:14:21 -0300

    projects1

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -7,20 +7,24 @@ export const Projects = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="max-w-5xl mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-            {" "}
-            Featured Projects
+        <div className="px-4 max-w-5xl mx-autow-full min-w-[300px] md:w-[800px] sm:w-2/3 p-6 text-center ">
+          <h2 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-sky-400 via-blue-600 to-purple-500">
+          {" "}
+            Proyectos
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2"> Cloud Platform</h3>
+              <h3 className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-sky-400 via-blue-600 to-purple-500"> Campeones del Mundo</h3>
               <p className="text-gray-400 mb-4">
-                Scalable cloud infrastructure management with real-time
-                monitoring and automated scaling.
+              Pagina Web E-commerce desarrollada en lenguaje JavaScript con tecnologías 
+              React en Front-End y Node.js en Back-End, una de las tecnologías más avanzadas 
+              en desarrollo web, para ofrecerte una experiencia de usuario fluida y rápida. 
+              Con una interfaz intuitiva y moderna, podrás navegar fácilmente por nuestras 
+              categorías, filtrar productos según tus necesidades y realizar tus compras de 
+              manera eficiente.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Node.js", "AWS", "Docker"].map((tech, key) => (
+                {["JavaScript","Vite", "React", "Redux", "Axios" , "Node.js", "Express", "Sequelize", "PosgreSQL"].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -34,10 +38,12 @@ export const Projects = () => {
 
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://www.youtube.com/watch?v=mTdL0whEhVg"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
-                  View Project →
+                  Ver Proyecto →
                 </a>
               </div>
             </div>
